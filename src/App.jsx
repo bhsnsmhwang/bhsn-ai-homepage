c0ec915fba45fb2ebed823ba3907f8163b815f2c
bhsnsmhwang <smhwang@bhsn.ai>
2021-11-04 23:45:09 +0900

    카카오지도 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -19,37 +19,8 @@ export const scroll = new SmoothScroll('a[href*="#"]', {
 
 const App = () => {
   const [landingPageData, setLandingPageData] = useState({});
-  const { kakao } = window;
   useEffect(() => {
     setLandingPageData(JsonData);
-    const container = document.getElementById('map');
-    const options = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667),
-        level: 3
-    };
-    const map = new kakao.maps.Map(container, options);
-        
-    // 주소-좌표 변환 객체를 생성합니다
-    var geocoder = new kakao.maps.services.Geocoder();
-    // 주소로 좌표를 검색합니다
-    geocoder.addressSearch('서울시 서초구 반포대로 18', function(result, status) {
-        // 정상적으로 검색이 완료됐으면 
-        if (status === kakao.maps.services.Status.OK) {
-            var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-            // 결과값으로 받은 위치를 마커로 표시합니다
-            var marker = new kakao.maps.Marker({
-                map: map,
-                position: coords
-            });
-            // 인포윈도우로 장소에 대한 설명을 표시합니다
-            var infowindow = new kakao.maps.InfoWindow({
-                content: '<div style="width:150px;text-align:center;color:black;padding:6px 0;">BHSN.AI</div>'
-            });
-            infowindow.open(map, marker);
-            // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-            map.setCenter(coords);
-        } 
-    });
   }, []);
 
   return (
